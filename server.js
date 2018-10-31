093099f50fc3d76920e6d4e685f306ac5b2928fc
dlee67 <dlee67@msudenver.edu>
2018-10-31 17:07:11 -0600

    Updated the server.js, needs peer review.

--- server.js
+++ server.js
@@ -9,12 +9,30 @@ app.listen(3005);
 
 var process=cmd.get('node');
 
-function executeCommand(command){
-  var hcCommand = command.toString()
+app.post('/hcinit', (req, res) => {
+  holochainInit(req.body.post); // Assuming the req.body.post contains the abosolute path specified from front-end.
+});
+
+app.post('/hcgenerate', (req, res) => {
+  holochainGenerate(req.body.post); // Assuming the req.body.post contains the abosolute path specified from front-end.
+});
+
+function holochainInit(absolutePath){
+  var hcCommand = "hc init " + absolutePath.toString();
   cmd.get(
-    hcCommand.toString(),
+    hcCommand,
     function(err, data, stderr){
-        console.log('the current working dir is : ',data)
+        console.log('Holochain project created at: ', absolutePath.toString());
+    }
+  );
+}
+
+function holochainGenerate(absolutePath){
+  var hcCommand = "hc generate " + absolutePath.toString();
+  cmd.get(
+    hcCommand,
+    function(err, data, stderr){
+        console.log('Holochain project created at: ', absolutePath.toString());
     }
   );
 }
